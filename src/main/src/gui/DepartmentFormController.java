fe63a5c05e34288e38ef5fd4943fd3bd20befca7
Gabson Roger <89322232+GabsonRogeer@users.noreply.github.com>
2022-07-28 23:16:06 -0300

    Validation Exception

--- src/main/src/gui/DepartmentFormController.java
+++ src/main/src/gui/DepartmentFormController.java
@@ -13,12 +13,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class DepartmentFormController implements Initializable {
 
@@ -69,6 +68,9 @@ public class DepartmentFormController implements Initializable {
             notifyDataChangeListeners();
             Utils.currentStage(event).close();
         }
+        catch (ValidationException e){
+            setErrorMesseges(e.getErrors());
+        }
         catch (DbException e){
             Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
         }
@@ -83,9 +85,18 @@ public class DepartmentFormController implements Initializable {
     private Department getFormData() {
         Department obj = new Department();
 
+        ValidationException exception = new ValidationException("Validation Error");
+
         obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+        if (txtName.getText() == null || txtName.getText().trim().equals("")){
+            exception.addError("name", "Field can't be empty");
+        }
         obj.setName(txtName.getText());
 
+        if(exception.getErrors().size() > 0) {
+            throw exception;
+        }
         return obj;
     }
 
@@ -112,5 +123,12 @@ public class DepartmentFormController implements Initializable {
         txtName.setText(entity.getName());
     }
 
+    private void setErrorMesseges(Map<String, String> errors){
+        Set<String> fields = errors.keySet();
+
+        if(fields.contains("name")){
+            labelErrorName.setText(errors.get("name"));
+        }
+    }
 
 }
